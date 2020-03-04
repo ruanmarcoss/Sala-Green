4b480cd6c905cdad7afe74dab8d8185170ea20cc
ruanmarcoss <silva.ruanmarcos@gmail.com>
2020-03-04 10:19:34 -0300

    Ex balde

--- Aula01/teste_balde/balde.py
+++ Aula01/teste_balde/balde.py
@@ -1,27 +1,32 @@
 from random import randint
 
-
 class Balde:
-    def __init__(self,__balde,__balde_atual):
+    def __init__(self):
         self.__balde = 0
         self.__balde_atual = 0
 
-    def set_volume_balde(self,volume_balde):
-        self.volume_balde = volume_balde
-        if type(self.volume_balde) == int and self.volume_balde > 0:
-            return self.volume_balde
+    def set_volume_balde(self, volume_balde):
+        if type(volume_balde) == int and volume_balde >=10 and volume_balde <= 50:
+            self.__balde = volume_balde
+            return self.__balde
+        else:
+            return False
 
     def get_volume_balde(self):
         return self.__balde
 
     def enchendo_balde(self,volume_agua):
-        if self.__balde == 0:
-
-
-
+        if type(volume_agua) == int and volume_agua > 0:
+            self.__balde_atual += volume_agua
+            if self.__balde_atual < self.__balde:
+                return 'Vazio'
+            elif self.__balde_atual == self.__balde:
+                return True
+            else:
+                return False
+        else:
+            return "ValorError"
 
     def sorteio(self):
-        self.volume = randint(10,50)
-        return self.volume
-
-
+            self.__balde = randint(10,50)
+            return self.__balde
